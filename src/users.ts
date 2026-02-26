526dac25a4991a5aa171f6888fe8a3d7f176d41b
sarah <81719479+akbarsar@users.noreply.github.com>
2026-02-25 20:28:34 -0500

    Update users.ts

--- src/users.ts
+++ src/users.ts
@@ -6,7 +6,26 @@ export const apiResponse: unknown = [
   { name: "Jim", age: 25 },
 ];
 
+function isValidUser(obj: unknown): obj is User {
+  if (typeof obj === "object" && obj != null && "name" in obj && "age" in obj) {
+    const user = obj as {name: unknown, age: unknown};
+    return typeof obj.name === string && typeof obj.age === number;
+  }
+  else {
+    return false;
+}
+
 export function getUsersData(): User[] {
+  if (!Array.isArray(apiResponse) return [];
+
+  return apiResponse
+  .map((item) => {
+    if (typeof obj === "object" && obj != null && "name" in obj && "age" in obj) {
+    const user = obj as {name: unknown, age: unknown};
+    let age: number | null = null; 
+
+    if (typeof obj.age === "string"
+  }
   return apiResponse as User[]; // intentionally unsafe
 }
 
